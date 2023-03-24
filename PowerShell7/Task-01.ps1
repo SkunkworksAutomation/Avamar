09556b39fdc1dc996d3854cc5da64e11656ed6ea
Clifford Rodriguez <77303096+automationftw@users.noreply.github.com>
2023-03-24 14:19:33 -0500

    place holder commit

--- PowerShell7/Task-01.ps1
+++ PowerShell7/Task-01.ps1
@@ -1,1 +1,14 @@
-# PLACE HOLDER
+<#
+    THIS CODE REQUIRES POWWERSHELL 7.x.(latest)
+    https://github.com/PowerShell/PowerShell/releases/tag/v7.3.3
+#>
+
+Import-Module .\dell.avamar.psm1 -Force
+$avamar = 'ave-01.vcorp.local'
+$pagesize = 25
+$Results = @()
+connect-restapi -Server $avamar
+
+<#
+   PLACE HOLDER
+#>
